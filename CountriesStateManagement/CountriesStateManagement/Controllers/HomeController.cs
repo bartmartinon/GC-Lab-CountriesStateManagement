0500d94e31711da2d22548da5da250e9fdcd12fd
Bart Martinon <barthelemy.martinon@gmail.com>
2020-11-16 17:31:03 -0500

    Base lab requirements done

--- CountriesStateManagement/CountriesStateManagement/Controllers/HomeController.cs
+++ CountriesStateManagement/CountriesStateManagement/Controllers/HomeController.cs
@@ -11,15 +11,68 @@ namespace CountriesStateManagement.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        List<Country> countryList = new List<Country>
         {
-            _logger = logger;
-        }
+            new Country("USA",new List<string> { "English", "Spanish", "Chinese"}, "Hello World!" , "The United States of America: The Land of the Free and the Home of the Brave.", new List<string> { "Red", "White", "Blue" }),
+            new Country("Spain", new List<string> { "Spanish", "Catalan" }, "Hola, Mundo!", "Spain: The Jewel of the Iberian Peninsula.", new List<string> { "Red", "Yellow" }),
+            new Country("France", new List<string> { "French", "Occitan", "Alsatian" }, "Bonjour, Monde!", "France: A land of art, food, drama, love and bizzare cinema full of dark themes.", new List<string> { "Blue", "White", "Red" }),
+            new Country("Japan", new List<string> { "Japanese" }, "Konichiwa, Warudo!", "Japan: The Sunrise Land, full of cherry blossoms.", new List<string> { "White", "Red" }),
+            new Country("Canada", new List<string> { "English", "French" }, "Hello, World!", "Canada: The Crown of North America. Maple syrup, snow, trees, and more snow!", new List<string> { "Red", "White" })
+        };
 
         public IActionResult Index()
         {
+            return View(countryList);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string CountrySelect)
+        {
+            foreach (Country c in countryList)
+            {
+                if (c.Name.Equals(CountrySelect))
+                {
+                    TempData["name"] = c.Name;
+                    TempData["languages"] = c.Languages;
+                    TempData["helloworld"] = c.HelloWorld;
+                    TempData["description"] = c.Description;
+                    TempData["colors"] = c.Colors;
+                }
+            }                
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult Details()
+        {
+            ViewBag.name = TempData.Peek("name");
+            ViewBag.languages = TempData.Peek("languages");
+            ViewBag.helloworld = TempData.Peek("helloworld");
+            ViewBag.description = TempData.Peek("description");
+            ViewBag.colors = TempData.Peek("colors");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Details(string Description)
+        {
+            ViewBag.name = TempData.Peek("name");
+            ViewBag.languages = TempData.Peek("languages");
+            ViewBag.helloworld = TempData.Peek("helloworld");
+            ViewBag.description = TempData.Peek("description");
+            ViewBag.colors = TempData.Peek("colors");
+
+            return RedirectToAction("DescriptionView");
+        }
+
+        public IActionResult DescriptionView()
+        {
+            ViewBag.name = TempData.Peek("name");
+            ViewBag.languages = TempData.Peek("languages");
+            ViewBag.helloworld = TempData.Peek("helloworld");
+            ViewBag.description = TempData.Peek("description");
+            ViewBag.colors = TempData.Peek("colors");
+
             return View();
         }
 
---
+++ CountriesStateManagement/CountriesStateManagement/Models/Country.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountriesStateManagement.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public List<string> Languages { get; set; }
+        public string HelloWorld { get; set; }
+        public string Description { get; set; }
+        public List<string> Colors { get; set; }
+
+        // Constructors
+        public Country(string Name, List<string> Languages, string HelloWorld, string Description, List<string> Colors)
+        {
+            this.Name = Name;
+            this.Languages = Languages;
+            this.HelloWorld = HelloWorld;
+            this.Description = Description;
+            this.Colors = Colors;
+        }
+    }
+}
